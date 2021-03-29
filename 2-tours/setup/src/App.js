bc2b508d671f7f386b1335f5bf9ca7319009db0f
Mark Talevski <mark.talevski+git@gmail.com>
2021-03-29 14:01:08 -0400

    Diplay tours list

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -4,8 +4,36 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+useEffect(() => {
+  fetch(url)
+  .then((response) => response.json())
+  .then((tours) => {
+    // const { id, name, info, image, price } = tours
+    setTours(tours)
+    setLoading(false)
+  })
+  .catch((error) => console.log(error))
+}, [])
+
+  if(loading) {
+      return(
+        <main>
+          <Loading />
+        </main>
+    )
+  }
+  return(
+    <main>
+      <Tours 
+        tours={tours}
+      />
+    </main>
+  )
 }
 
 export default App
